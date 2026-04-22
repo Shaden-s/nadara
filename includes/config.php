ef7a0386ed7524cab8d3f83aeed4580f7c7f5545
Sarah-001 <sarakhalid5000@gmail.com>
2026-04-22 20:04:13 +0300

    Edit

--- includes/config.php
+++ includes/config.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "nadara");
+$conn = mysqli_connect("localhost", "root", "", "nadara", 3307);
 
 if (!$conn) {
     die(mysqli_connect_error());
